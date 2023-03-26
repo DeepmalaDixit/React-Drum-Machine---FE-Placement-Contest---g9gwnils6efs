3c6f965adb9ab17ae819e0c6c1a8408b5220321a
Deepmala <117112464+DeepmalaDixit@users.noreply.github.com>
2023-03-26 15:35:19 +0530

    Update Pads.js

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,31 +1,34 @@
 import React from "react";
 import Pad from "./Pad";
-import {bank1} from "./App"
+import { bank1 } from "./App";
 
+function Pads({ power, volume }) {
+  const keypadCode = Object.keys(bank1);
 
-function Pads() {
-    const keypadCode = Object.keys(bank1);
+  const playSound = (e) => {
+    const audio = document.getElementById(e.target.innerText);
+    audio.volume = volume;
+    audio.currentTime = 0;
+    audio.play();
+    document.getElementById("display").innerText = bank1[e.target.innerText].name;
+  };
 
+  return (
+    <div id="div-pads">
+      {keypadCode.map((pad, idx) => {
+        return (
+          <Pad
+            id={pad + idx}
+            key={pad + idx}
+            handleClick={playSound}
+            element={pad}
+            power={power}
+          />
+        );
+      })}
+      <div id="display">Play a sound!</div>
+    </div>
+  );
+}
 
-    const playSound = e => {
-    }
-
-
-    return (
-      <div id='div-pads'>
-        {keypadCode.map((pad, idx) => {
-          console.log(pad + idx)
-          return (
-            <Pad
-              id={pad+idx}
-              key={pad+idx}
-              handleClick={playSound}
-              element={pad} />
-          );
-        })}
-        <div id='display'> Show the name of current audio here </div>  
-      </div>
-    )
-  }
-
-  export default Pads ; 
+export default Pads;
