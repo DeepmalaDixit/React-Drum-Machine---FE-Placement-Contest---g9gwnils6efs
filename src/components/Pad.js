69d6dbe18c2757f61a16396eefd1bc7b263b4f29
Deepmala <117112464+DeepmalaDixit@users.noreply.github.com>
2023-03-26 15:34:27 +0530

    Update Pad.js

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,20 +1,26 @@
 import React from "react";
-import {bank1} from "./App"
+import { bank1 } from "./App";
 
-function Pad({handleClick, power, backgroundStyle, element, id }) {
-    return (
-      <button 
-        data-tag={id}
-        type='button'
-        className='drum-pad' 
-        onClick={handleClick} 
-        id={bank1[element]} 
-        disabled={!power} 
-        style={{background: `${backgroundStyle}`}}>
-        {element}
-        <audio id={element} src={bank1[element].source} className='clip'></audio>
-      </button>
-    );
-  }
-  
-  export default Pad ; 
+function Pad({ handleClick, power, element, id }) {
+  const backgroundStyle = power ? "orange" : "gray";
+  return (
+    <button
+      data-tag={id}
+      type="button"
+      className="drum-pad"
+      onClick={handleClick}
+      id={bank1[element].name}
+      disabled={!power}
+      style={{ background: `${backgroundStyle}` }}
+    >
+      {element}
+      <audio
+        id={element}
+        src={bank1[element].source}
+        className="clip"
+      ></audio>
+    </button>
+  );
+}
+
+export default Pad;
